a8f689fc41691c8fdf15b4ac8fa5d1f17b786d3e
serg <sergey126992@mail.ru>
2018-01-23 17:31:30 +0300

    project shop first commit

--- backend/controllers/AuthController.php
+++ backend/controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
     public function __construct($id, $module, AuthService $service, $config = [])
     {
         parent::__construct($id, $module, $config);
-        $this->authService = $service;
+        $this->authService =  $service;
     }
 
     /**
